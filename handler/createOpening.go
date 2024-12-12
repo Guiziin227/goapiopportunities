90b751306e5470c9bd686438371a39e9a6f67280
Guiziin227 <gh63388@gmail.com>
2024-12-12 12:51:41 -0300

    feat: createOpening logic

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -1,15 +1,36 @@
 package handler
 
 import (
+	"github.com/Guiziin227/goapiopportunities.git/schemas"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func CreateOpeningHandler(ctx *gin.Context) {
 	request := CreateOpeningRequest{}
 
 	ctx.Bind(&request)
-	if err := db.Create(&request).Error; err != nil {
-		logger.Errorf("Error creating a new opening request: %v", err.Error())
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("Error create opening %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
 		return
 	}
+
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&opening).Error; err != nil {
+		logger.Errorf("Error creating a new opening request: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	sendSuccess(ctx, "createOpening", opening)
 }
